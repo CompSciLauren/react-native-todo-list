4e0fc24591e8f67dfd25300f1a213a5e85cf1d4b
Lauren Stephenson <compscilauren@gmail.com>
2019-10-13 18:34:54 -0500

    Add gradient background

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { primaryGradientArray } from './utils/Colors';
+export default class App extends React.Component {
+  render() {
+    return (
+      <LinearGradient colors={primaryGradientArray} style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </LinearGradient>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
