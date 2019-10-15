d2af9a054f7429babb1eca9f7d90c229d9a3c56e
Lauren Stephenson <compscilauren@gmail.com>
2019-10-15 10:37:23 -0500

    Add input functionality

--- App.js
+++ App.js
@@ -3,24 +3,43 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { primaryGradientArray } from './utils/Colors';
 import Header from './components/Header';
+import Input from './components/Input';
 
 const headerTitle = 'To Do';
 export default class App extends React.Component {
+  state = {
+    inputValue: '',
+  };
+  newInputValue = value => {
+    this.setState({
+      inputValue: value,
+    });
+  };
   render() {
+    const { inputValue } = this.state;
     return (
       <LinearGradient colors={primaryGradientArray} style={styles.container}>
+        <StatusBar barStyle="light-content" />
         <View style={styles.centered}>
           <Header title={headerTitle} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Input inputValue={inputValue} onChangeText={this.newInputValue} />
         </View>
       </LinearGradient>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   centered: {
     alignItems: 'center',
+  },
+  inputContainer: {
+    marginTop: 40,
+    paddingLeft: 15,
   },
 });
