46e2ca714afac6ab6fbf7a5125fec75c09239640
Lauren Stephenson <compscilauren@gmail.com>
2019-10-17 17:37:52 -0500

    Change input text placeholder

--- components/Input.js
+++ components/Input.js
@@ -7,7 +7,7 @@ const Input = ({ inputValue, onChangeText, onDoneAddItem }) => (
     style={styles.input}
     value={inputValue}
     onChangeText={onChangeText}
-    placeholder="Type here to add note."
+    placeholder="Add a task"
     placeholderTextColor={inputPlaceholder}
     multiline={true}
     autoCapitalize="sentences"
